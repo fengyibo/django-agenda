977dd78244c50b4cdbb3eb45132bec291169082d
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2010-10-11 11:28:55 -0600

    reparando cosas del ultimo commit XD

--- calendar/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms as forms
-from django.forms.extras.widgets import *
-from models import *
-
-class EditForm(forms.ModelForm):       
-    class Meta:
-        model = Citas        
- 
